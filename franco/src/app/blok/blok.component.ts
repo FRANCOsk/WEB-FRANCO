95d17e433a8b59cea93e284ca1d0d6d9de97d9ec
FRANK <developer71@azet.sk>
2026-07-20 07:26:47 +0200

    fix: keep BlokComponent NgModule-based

--- franco/src/app/blok/blok.component.ts
+++ franco/src/app/blok/blok.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blok',
+  standalone: false,
   templateUrl: './blok.component.html',
   styleUrls: ['./blok.component.css']
 })
 export class BlokComponent implements OnInit {
+  @Input() header = 'Empty header';
+  @Input() text = 'Empty text';
 
-  @Input() 
-  header: string ="Empty header";
-  @Input()
-  text: string="Empty text";
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
