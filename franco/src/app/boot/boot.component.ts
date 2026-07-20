1ae0735733af9d153b1fa61dac47df824f6fd395
FRANK <developer71@azet.sk>
2026-07-20 07:27:22 +0200

    fix: keep BootComponent NgModule-based

--- franco/src/app/boot/boot.component.ts
+++ franco/src/app/boot/boot.component.ts
@@ -2,14 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-boot',
+  standalone: false,
   templateUrl: './boot.component.html',
   styleUrls: ['./boot.component.css']
 })
 export class BootComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
